1161b71597371efa76af803459eeb6c1ae6cacf5
Khushi Dusad <khushidusad2004@gmail.com>
2025-04-14 20:26:58 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const uri ="mongodb+srv://Khushi:Khushi534@cluster0.tmfczui.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = //mongodb cluster uri
 
 module.exports.connect = () =>{
     mongoose.connect(uri).then((res)=>console.log("MongoDB connected successfully")).catch((err)=>console.log("Error: ", err))
