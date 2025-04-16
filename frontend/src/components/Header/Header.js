812d6d460603d1a8e9d65170c2337f0eb20d222c
Khushi Dusad <khushidusad2004@gmail.com>
2025-04-16 11:10:21 +0530

    Update Header.js

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -4,7 +4,7 @@ import SearchIcon from "@mui/icons-material/Search";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import { Link, useNavigate } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { selectUser, logout } from "../../features/userSlice"; // update path as needed
+import { selectUser, logout } from "../../features/userSlice";
 
 function Header() {
   const user = useSelector(selectUser);
